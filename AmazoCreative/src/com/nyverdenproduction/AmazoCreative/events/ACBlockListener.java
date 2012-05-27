3224b2f16bebac0f8f99dc31584f4f2020314281
Vincent Deloso <mitsugaru@gmail.com>
2012-05-27 19:02:24 -0400

    Implement ignore node. Deny destruction. Check that block placed is actually inside the region.

--- AmazoCreative/src/com/nyverdenproduction/AmazoCreative/events/ACBlockListener.java
+++ AmazoCreative/src/com/nyverdenproduction/AmazoCreative/events/ACBlockListener.java
@@ -5,10 +5,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 import com.nyverdenproduction.AmazoCreative.AmazoCreative;
 import com.nyverdenproduction.AmazoCreative.Item;
+import com.nyverdenproduction.AmazoCreative.permissions.PermissionNode;
 import com.sk89q.worldguard.protection.ApplicableRegionSet;
 
 public class ACBlockListener implements Listener
@@ -21,6 +23,40 @@ public class ACBlockListener implements Listener
 	}
 
 	@EventHandler(priority = EventPriority.NORMAL)
+	public void onBlockDestroy(final BlockBreakEvent event)
+	{
+		if (event.isCancelled() || event.getPlayer() == null
+				|| event.getBlock() == null)
+		{
+			return;
+		}
+		final Player player = event.getPlayer();
+		// Check for ignore permission node
+		if (plugin.getPermissionsHandler().checkPermission(player,
+				PermissionNode.IGNORE))
+		{
+			return;
+		}
+		// check if in applicable world
+		if (!plugin.getConfigHandler().getRootConfig().worlds.contains(player
+				.getWorld().getName()))
+		{
+			return;
+		}
+		final ApplicableRegionSet regionSet = plugin.getWorldGuard()
+				.getRegionManager(event.getBlock().getWorld())
+				.getApplicableRegions(event.getBlock().getLocation());
+		// Check if its not in a region
+		if (regionSet.size() <= 0)
+		{
+			// Deny
+			event.setCancelled(true);
+			player.sendMessage(ChatColor.RED + AmazoCreative.TAG
+					+ " Cannot destroy outside of a region.");
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
 	public void onBlockPlace(final BlockPlaceEvent event)
 	{
 		if (event.isCancelled() || event.getPlayer() == null
@@ -29,6 +65,12 @@ public class ACBlockListener implements Listener
 			return;
 		}
 		final Player player = event.getPlayer();
+		// Check for ignore permission node
+		if (plugin.getPermissionsHandler().checkPermission(player,
+				PermissionNode.IGNORE))
+		{
+			return;
+		}
 		final Item item = new Item(event.getBlock().getTypeId(), event
 				.getBlock().getData(), (short) 0);
 		// check if in applicable world
@@ -61,24 +103,38 @@ public class ACBlockListener implements Listener
 		}
 		else
 		{
-			// Check the player's current limit for the block
-			int limit = plugin.getConfigHandler().getStorageConfig()
-					.getPlayerLimit(player.getName(), item);
-			if (limit >= plugin.getConfigHandler().getValuesConfig().items
-					.get(item).limit)
+			regionSet = plugin.getWorldGuard()
+					.getRegionManager(event.getBlockPlaced().getWorld())
+					.getApplicableRegions(event.getBlockPlaced().getLocation());
+			if (regionSet.size() > 0)
 			{
-				// deny if player is at / above limit
-				event.setCancelled(true);
-				// TODO send message to player
-				player.sendMessage(ChatColor.RED + AmazoCreative.TAG
-						+ " Hit limit for item!");
+				// Check the player's current limit for the block
+				int limit = plugin.getConfigHandler().getStorageConfig()
+						.getPlayerLimit(player.getName(), item);
+				if (limit >= plugin.getConfigHandler().getValuesConfig().items
+						.get(item).limit)
+				{
+					// deny if player is at / above limit
+					event.setCancelled(true);
+					// send message to player
+					player.sendMessage(ChatColor.RED + AmazoCreative.TAG
+							+ " Hit limit for item!");
+				}
+				else
+				{
+					// increment player value if event is not denied
+					plugin.getConfigHandler().getStorageConfig()
+							.setPlayerLimit(player.getName(), item, ++limit);
+				}
 			}
 			else
 			{
-				// increment player value if event is not denied
-				plugin.getConfigHandler().getStorageConfig()
-						.setPlayerLimit(player.getName(), item, ++limit);
+				// Deny
+				event.setCancelled(true);
+				player.sendMessage(ChatColor.RED + AmazoCreative.TAG
+						+ " Must build inside a region.");
 			}
+
 		}
 	}
 }
